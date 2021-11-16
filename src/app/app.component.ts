18d64e830016daa363c616e6ab81facc562cd5d8
Jimi Flynn <jimi@suremarketingsolutions.com>
2021-11-16 12:23:37 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tlgyo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tlgyo-app';
+}
