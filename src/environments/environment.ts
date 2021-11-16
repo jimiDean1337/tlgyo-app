f1622e32d6a438ebdd970295b5590351ea932c02
Jimi Flynn <jimi@suremarketingsolutions.com>
2021-11-16 12:33:38 -0500

    init commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCQhk3M4VgCmc8I4fj8P_sh3_q5NQsg88A",
+    authDomain: "tlgyo-d60f5.firebaseapp.com",
+    projectId: "tlgyo-d60f5",
+    storageBucket: "tlgyo-d60f5.appspot.com",
+    messagingSenderId: "728479250560",
+    appId: "1:728479250560:web:012fcc419b05427729ce65",
+    measurementId: "G-XBXTM9HGEF"
+  }
 };
 
 /*
